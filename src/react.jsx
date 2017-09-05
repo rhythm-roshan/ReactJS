3aef69aeb319392a5c2a330874fad1b308ebac6f
rhythm-roshan <rhythm.roshan@gmail.com>
2017-09-05 12:15:09 +0530

    Print Hello In browser

--- src/react.jsx
+++ src/react.jsx
@@ -1,3 +1,27 @@
-console.log("Hello World");
+import React from "react";
+import ReactDOM from "react-dom";
 
-// We can put ES6, JSX code here
+
+//React Component
+
+class HelloWorld extends React.Component {
+
+    render() {
+        //react element
+        return (
+            <div><h1>hello rhythm..!!! Welcome</h1></div>
+        )
+    }
+}
+
+//Instantiate
+//Create a react Element from a react component
+//JSX style
+//HW- pre JSX
+var r_e = <div/>
+var r_e = <HelloWorld/>
+
+
+var node = document.getElementById("app");
+
+ReactDOM.render(r_e,node);
